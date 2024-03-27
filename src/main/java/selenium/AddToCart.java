2450d8d2db256e27170249f08f2d155bcf339ac3
piyush1160 <piyushbhatia534@gmail.com>
2024-03-27 18:30:29 +0530

    update code

--- src/main/java/selenium/AddToCart.java
+++ src/main/java/selenium/AddToCart.java
@@ -4,14 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class AddToCart {
+    static String successfullyApplied = "Code applied ..!";
 
-    static String[] productList = {"Cucumber","Brocolli","Beetroot"};
-
-
+    static String[] productList = {"Cucumber","Brocolli","Beetroot","Carrot","Tomato"};
 
     public  static void addToCart(WebDriver driver) throws  Exception{
         List<WebElement> prodlist = driver.findElements(By.cssSelector("h4.product-name"));
@@ -39,16 +40,34 @@ public class AddToCart {
 
             }
         }
-
-        Thread.sleep(2000);
+        proceddToCart(driver);
+      //  Thread.sleep(2000);
         driver.quit();
+    }
+
+
+    public static void  proceddToCart(WebDriver driver) throws Exception{
+        driver.findElement(By.cssSelector(".cart-icon")).click();
+
+        // proceed to checkout ..
+      driver.findElement(By.xpath("//*[@id=\'root\']/div/header/div/div[3]/div[2]/div[2]/button")).click();
+     // Thread.sleep(1000);
+      driver.findElement(By.cssSelector("input[placeholder='Enter promo code']")).sendKeys("rahulshettyacademy");
+      driver.findElement(By.cssSelector(".promoBtn")).click();
+
+
+        System.out.println(driver.findElement(By.cssSelector(".promoInfo")).getText());
+        String actual = driver.findElement(By.cssSelector(".promoInfo")).getText();
+
+        Assert.assertEquals(successfullyApplied,actual,"succesfully applied ");
+      driver.findElement(By.cssSelector("body > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > button:nth-child(14)")).click();
     }
 
     @Test
     public static void addingToCart() throws Exception {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-
+       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         addToCart(driver);
 
